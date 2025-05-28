9ca2bfd622a9dc55af567ddc08948f31b7adbac1
kimdeokyoung <dudejrss@naver.com>
2025-05-28 16:51:38 +0900

    feat: Seller

--- api/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./index');
-
-app.listen(3001, () => {
-  console.log('API listening on port 3001');
-});
--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,26 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [msg, setMsg] = useState('');
+  const [sellers, _] = useState([]);
 
   useEffect(() => {
     fetch('/api/hello')
       .then(res => res.json())
       .then(data => setMsg(data.message));
+
+
+    fetch('/api/seller')
+      .then(res => res.json())
+      .then(({sellers}) => setSellers(sellers))
   }, []);
 
-  return <h1>{msg}</h1>;
+  return (
+    <div>
+      {
+        sellers && sellers.map(seller=><div>{seller.id}</div>)
+      }
+    </div>
+    );
 }
 
 export default App;
